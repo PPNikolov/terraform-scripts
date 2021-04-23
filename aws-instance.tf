624f93bce8d85a9ea80bfe32a20f3075975632b2
Петър Николов <petar.nikkollov@gmail.com>
2021-04-23 16:16:48 +0300

    [AWS INSTANCE] Allow HTTP port on the instance
    Allowing HTTP port and trying to install nginx

--- aws-instance.tf
+++ aws-instance.tf
@@ -7,6 +7,24 @@ provider "aws" {
 resource "aws_instance" "web-server" {
   ami = "ami-0019f18ee3d4157d3"
   instance_type = "t2.micro"
+  vpc_security_group_ids = [ aws_security_group.web-server.id ]
+  user_data = <<-EOF
+                sudo yum -y install epel-release
+                sudo yum -y install nginx
+                sudo systemctl enable nginx
+                sudo systemctl start nginx
+                EOF
+}
+
+resource "aws_security_group" "web-server" {
+  name = "web-srv1"
+  
+  ingress {
+    protocol = "tcp"
+    from_port = 80
+    to_port = 80
+    cidr_blocks = [ "0.0.0.0/0" ] 
+  }
 }
 
 output "instance_ips" {
