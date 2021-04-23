e74ac400723b9cd1511e5fb8385974c070f814f8
Петър Николов <petar.nikkollov@gmail.com>
2021-04-23 17:12:10 +0300

    [AWS INSTANCE] providing and allowing ssh
    Provisioning ssh pub key + enabling the option to connect with ssh

--- aws-instance.tf
+++ aws-instance.tf
@@ -17,13 +17,31 @@ resource "aws_instance" "web-server" {
   ami = "ami-0019f18ee3d4157d3"
   instance_type = "t2.micro"
   vpc_security_group_ids = [ aws_security_group.web-server.id ]
-#  user_data = <<-EOF
-#                #!/bin/bash
-#                sudo yum -y install epel-release
-#                sudo yum -y install nginx
-#                sudo systemctl enable nginx
-#                sudo systemctl start nginx
-#                EOF
+  user_data = <<-EOF
+
+    Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [users-groups, once]
+users:
+  - name: root
+    ssh-authorized-keys: 
+    - PublicKeypair
+
+EOF
+}
+
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC+EHkcujhnlJqi9hK5uEAT/o4xp8cuYtbtgyvZo9diIxhGvOb1ZKSk2ne3+XBH8HJvuOOqmhu0KwnL+tAGF6Q1LCoGjFPZLF+xrwvQeEuE7VCzTRJRvtA6fhrVc8WQPl4Ib1B8LBwDS+/JJRVpiNRpcjUbZrSSDyvmTF6V6FRaJie5AsCYNENXXV+4WgqmqL7cR8S49t/8tGpEVeJMk+HIXpzDzJxJ2U5zpRV9FsHEiTM8KavhvlHEXP5AJqjS/mEfdKBKt7/xwUij7JzU51OccWyBXdF0EBb/K4gyW9h0m8oCrYu44J5SqCfCSYkdES5rEXZrxA5iWIUssFKLcNGzL9ifHO1PC/NMAgOQ+HjAvvAjjHA83ACl9y4SEwaRnEMokhFStoFniWUHZ9E45w6+0YseW60Qno4j2r+MtdyRUPEN/a+WFwSk3KUkzNr5lZ4F1tngAYziib7iVWHgI/D9xH1BgCLYS6JpDf5R1VTjhMa9DFFF2/1+6PeE+c2Yit7FToZfJYTiYDrUigIZdrP+pQlCyjyRlxbfxbIyEs/RDIG+t2LanCp9ZlgY6txWll4RFPgZE1BW/PqXFfrTcNf75VeKKJsysEjJrweWxU6fsGeIJDXikrAMHAc3AGGy1inrPzW9Ww2/zLwvkOn0RBPIW33payVcWhe74S6Shq7WyQ== petar.nikolov@softwaregroup.com"
 }
 
 resource "aws_security_group" "web-server" {
